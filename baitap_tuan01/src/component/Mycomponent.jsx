37803601f4b123e22639345bf56adced6882a7da
H82M19\Student <Student>
2025-02-18 15:54:10 +0700

    first commit

---
+++ baitap_tuan01/src/component/Mycomponent.jsx
@@ -0,0 +1,12 @@
+import React  from "react";
+class Mycomponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>My first component</h1>
+            </div>
+        )
+    }
+}
+
+export default Mycomponent
